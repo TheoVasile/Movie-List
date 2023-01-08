9c3c53e8c4b3b214e6edf42a51ac0f503af179a6
TheoVasile <theo.vasile50@yahoo.com>
2023-01-07 22:51:54 -0500

    Initial Commit

---
+++ Movie List/Movie_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_ListApp.swift
+//  Movie List
+//
+//  Created by Theo Vasile on 2023-01-07.
+//
+
+import SwiftUI
+
+@main
+struct Movie_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
