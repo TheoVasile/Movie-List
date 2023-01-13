fdaf6fe36a84219f512d51d23be3ad23f20f5675
TheoVasile <theo.vasile50@yahoo.com>
2023-01-12 22:55:56 -0500

    Popup when pressing navbar on home screen to add new movie list

--- Movie List/View/Home.swift
+++ Movie List/View/Home.swift
@@ -8,28 +8,49 @@
 import SwiftUI
 
 struct Home: View {
+    
+    @State var showPopup: Bool = false
+    
     var body: some View{
         let db = DataAccess()
         let array = db.getLists() ?? []
-        NavigationView{
-            VStack{
-                if array.count == 0{
-                    Text("No Lists, Add One Now!")
-                }
-                ForEach(array, id: \.self) {list in
-                    NavigationLink(list, destination: ListView())
-                }
-            }
-            .navigationTitle("Movie Lists")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar{
-                ToolbarItem(placement: .navigationBarTrailing){
-                    Button{
-                        
-                    } label: {
-                        Image(systemName: "plus")
+        ZStack{
+            NavigationView{
+                VStack{
+                    if array.count == 0{
+                        Text("No Lists, Add One Now!")
+                    }
+                    ForEach(array, id: \.self) {list in
+                        NavigationLink(list, destination: ListView())
                     }
                 }
+                .navigationTitle("Movie Lists")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar{
+                    ToolbarItem(placement: .navigationBarTrailing){
+                        Button{
+                            withAnimation{showPopup.toggle()}
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                    }
+                }
+            }
+            .popupNavigationView(horizontalPadding: 40, show: $showPopup){
+                Text("ListName")
+                    .disableAutocorrection(true)
+                    .navigationTitle("Popup")
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbar{
+                        ToolbarItem(placement: .navigationBarLeading){
+                            Button("Close"){
+                                withAnimation{
+                                    showPopup.toggle()
+                                }
+                            }
+                        }
+                    }
+                
             }
         }
     }
