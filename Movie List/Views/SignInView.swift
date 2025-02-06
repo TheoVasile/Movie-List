9d3d4558972bce429ed69381aadf69388ccadac1
TheoVasile <theo.vasile50@yahoo.com>
2025-02-06 03:18:39 -0500

    completed sign up ui

--- Movie List/Views/SignInView.swift
+++ Movie List/Views/SignInView.swift
@@ -13,28 +13,43 @@ struct SignInView: View {
     @State private var password: String = ""
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("Sign In")
-                TextField("Email", text: $email)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                SecureField("Password", text: $password)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                NavigationLink(destination: AccountView()) {
-                    Text("Log In")
-                }
-                HStack {
-                    Text("No account?")
-                        .foregroundColor(.gray)
-                    NavigationLink(destination: SignUpView()) {
-                        Text("Sign Up")
-                            .foregroundColor(.blue)
-                            .bold()
+            ZStack{
+                Color(UIColor.systemGray6)
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    Spacer()
+                    Text("Sign In")
+                    TextField("Email", text: $email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    NavigationLink(destination: AccountView()) {
+                        Text("Log In")
+                            .foregroundColor(.white)
+                            .padding(10)
                     }
+                    .background(.blue)
+                    .cornerRadius(20)
+                    .padding()
+                    HStack {
+                        Text("No account?")
+                            .foregroundColor(.gray)
+                        NavigationLink(destination: SignUpView()) {
+                            Text("Sign Up")
+                                .foregroundColor(.blue)
+                                .bold()
+                        }
+                    }
+                    Spacer()
                 }
+                .background(.white)
+                .cornerRadius(20)
+                .padding(20)
             }
         }
+        .navigationBarBackButtonHidden(true)
     }
 }
     
--- Movie List/Views/SignUpView.swift
+++ Movie List/Views/SignUpView.swift
@@ -9,8 +9,55 @@ import Foundation
 import SwiftUI
 
 struct SignUpView: View {
+    @State private var email: String = ""
+    @State private var password1: String = ""
+    @State private var password2: String = ""
+    @State private var name: String = ""
     var body: some View {
-        Text("Sign Up")
+        NavigationStack {
+            ZStack{
+                Color(UIColor.systemGray6)
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    Spacer()
+                    Text("Sign Up")
+                    TextField("Name", text: $name)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    TextField("Email", text: $email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    SecureField("Password", text: $password1)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    SecureField("Re-enter Password", text: $password2)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    NavigationLink(destination: AccountView()) {
+                        Text("Create Account")
+                            .foregroundColor(.white)
+                            .padding(10)
+                    }
+                    .background(.blue)
+                    .cornerRadius(20)
+                    .padding()
+                    HStack {
+                        Text("Already have an account?")
+                            .foregroundColor(.gray)
+                        NavigationLink(destination: SignInView()) {
+                            Text("Sign In")
+                                .foregroundColor(.blue)
+                                .bold()
+                        }
+                    }
+                    Spacer()
+                }
+                .background(.white)
+                .cornerRadius(20)
+                .padding(20)
+            }
+        }
+        .navigationBarBackButtonHidden(true)
     }
 }
 
